024569dcf9553281b1fb2dee114ad016691b1196
BenGregory23 <bengregorygbr@gmail.com>
2024-01-07 14:57:08 +0100

    stuff

--- app/components/projects/project-button.tsx
+++ app/components/projects/project-button.tsx
@@ -4,9 +4,13 @@ import { Button } from "../ui/button"
 
 export default function ProjectButton(props: { title: string, id: number, setProject: any, project: object }) {
     return (
-        <Button onClick={()=>props.setProject(props.project)} variant={"outline"} className="flex flex-col justify-center items-center p-3 hover:outline-none">
+        <Button onClick={()=>props.setProject(props.project)} variant={"outline"} className="flex flex-col justify-center items-start py-3 px-5 hover:outline-none">
             {props.title}
+          
             
+        
+            {/* <p className="text-sm font-light text-muted-foreground">{props.project.subtitle}</p> */}
+           
         </Button>
     )
 }
