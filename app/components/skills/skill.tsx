993839d6d44c571c7091f68d1a705bc12bff51aa
Ben GREGORY <bengregorygbr@gmail.com>
2024-01-12 00:47:55 +0100

    started skills implementation

---
+++ app/components/skills/skill.tsx
@@ -0,0 +1,24 @@
+import { stack_type } from "~/utils/stack_type"
+
+interface Skill {
+    name:string,
+    image_path: string,
+    isMainStack: boolean,
+    type: stack_type
+}
+
+export async function loader(){
+
+    
+
+}
+
+export const Skill = (skill:Skill) =>{
+
+    return (
+        <div>
+            TODO
+        </div>
+    )
+
+}
---
+++ app/utils/stack_type.ts
@@ -0,0 +1,6 @@
+export enum stack_type {
+    FRONTEND,
+    BACKEND,
+    DATABASE,
+    TOOLS
+}
