58cd850e8fdcf508f5cad660e2f6d7283ada9b7a
BenGregory23 <bengregorygbr@gmail.com>
2024-01-29 18:35:58 +0100

    Add Sticker component

---
+++ app/components/stickers/sticker.tsx
@@ -0,0 +1,18 @@
+const Sticker = ({
+  text,
+  title,
+  className,
+}: {
+  text: string;
+  title: string;
+  className: string;
+}) => {
+  return (
+    <div className={className}>
+   
+      
+    </div>
+  );
+};
+
+export default Sticker;
